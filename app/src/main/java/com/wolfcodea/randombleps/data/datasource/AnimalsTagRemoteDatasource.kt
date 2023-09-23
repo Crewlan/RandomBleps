f5a753ca7b1356bac2976437f1f99fd9ffc0f8a7
Rodrigo Amancio <rodrigoa.work@gmail.com>
2023-09-23 00:28:16 -0300

    Call api and receive name tag for animals

--- app/src/main/java/com/wolfcodea/randombleps/data/datasource/AnimalsTagRemoteDatasource.kt
+++ app/src/main/java/com/wolfcodea/randombleps/data/datasource/AnimalsTagRemoteDatasource.kt
@@ -1,28 +1,34 @@
 package com.wolfcodea.randombleps.data.datasource
 
+import com.wolfcodea.randombleps.core.colors.Constants
 import com.wolfcodea.randombleps.core.endpoints.Endpoints
 import com.wolfcodea.randombleps.core.exceptions.DataOrException
 import com.wolfcodea.randombleps.data.models.AnimalsTags
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 interface IAnimalsTagApi {
+    @Headers(
+        "X-RapidAPI-Key: ${Constants.API_KEY}",
+        "X-RapidAPI-Host: ${Constants.API_HOST}"
+    )
     @GET(Endpoints.ANIMAL_TAGS)
     suspend fun getAnimalTags(): AnimalsTags
 }
 
-class AnimalsTagRemoteDatasource @Inject constructor(private val api: IAnimalsTagApi){
+class AnimalsTagRemoteDatasource @Inject constructor(private val api: IAnimalsTagApi) {
     private val dataOrException = DataOrException<AnimalsTags, Boolean, Exception>()
 
-    suspend fun  getAnimalsTag():DataOrException<AnimalsTags, Boolean, java.lang.Exception>{
+    suspend fun getAnimalsTag(): DataOrException<AnimalsTags, Boolean, java.lang.Exception> {
         try {
             dataOrException.loading = true
             dataOrException.data = api.getAnimalTags()
-            if(dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
 
-        }catch (exception: Exception){
+        } catch (exception: Exception) {
             dataOrException.e = exception
         }
         return dataOrException
--- app/src/main/java/com/wolfcodea/randombleps/presentation/components/AnimalsHome.kt
+++ app/src/main/java/com/wolfcodea/randombleps/presentation/components/AnimalsHome.kt
@@ -1,11 +1,36 @@
 package com.wolfcodea.randombleps.presentation.components
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
 import com.wolfcodea.randombleps.data.models.AnimalsTagViewModel
 
-@Preview
-@Composable
-fun AnimalsHome(viewModel: AnimalsTagViewModel? = null){
 
+@Composable
+fun AnimalsHome(viewModel: AnimalsTagViewModel){
+    val animalsTag = viewModel.data.value.data?.toMutableList()
+
+    if(viewModel.data.value.loading == true){
+        Box(modifier = Modifier.fillMaxSize()){
+            CircularProgressIndicator()
+        }
+    }else{
+        Log.d("TAG", "AnimalsHome: $animalsTag")
+        BuildLazyAnimals(animalsTag)
+    }
+}
+
+@Composable
+fun BuildLazyAnimals(animals:  List<String>? ){
+    LazyColumn{
+        items(items = animals!!){
+            animal -> Text(text = "$animal")
+        }
+    }
 }
